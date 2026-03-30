bbd67e2dd1c8e2b0fc85c9db1f4955d81b280210
kokiuchida <ucchii84@gmail.com>
2026-03-31 00:39:56 +0900

    stock.py Mock完成

--- stock.py
+++ stock.py
@@ -24,61 +24,65 @@ def get_data(days, tickers):
     for company in tickers.keys():
         tkr = yf.Ticker(tickers[company])
         hist = tkr.history(period=f'{days}d')
-        hist = hist.index.strftime('%d %B %Y')
+        hist.index = hist.index.strftime('%d %B %Y')
         hist = hist[['Close']]
         hist.columns = [company]
         hist = hist.T
         hist.index.name = 'Name'
-        df = pd.cancat([df, hist])
+        df = pd.concat([df, hist])
     return df
 
-st.sidebar.write("""
-## 株価の指定
-""")
 
-ymin, ymax = st.sidebar.slider(
-    '範囲を指定してください',
-    0.0, 3500.0, (0.0, 3500,0)
-)
+try:
+    st.sidebar.write("""
+    ## 株価の指定
+    """)
 
-tickers = {
-    'apple':'AAPL',
-    'facebook':'FB',
-    'google':'GOOGL',
-    'microsoft':'MSFT',
-    'netflix':'NFLX',
-    'amazon':'AMZN'
-}
-
-df = get_data(days, tickers)
-companies = st.multiselect(
-    '会社名を選択してください',
-    list(df.index),
-    ['google','amazon','apple','facebook']
-)
-
-if not companies:
-    st.error('少なくとも1社は選んでください')
-else:
-    data = df.loc[companies]
-    st.write("### 株価(USD)", data.sort_index())
-    data.T.reset_index()
-    data = pd.melt(data, id_vars=['Date']).rename(
-        columns={'value': 'Stock Prices(USD)'}
+    ymin, ymax = st.sidebar.slider(
+        '範囲を指定してください',
+        0.0, 1500.0, (0.0, 750.0)
     )
 
-    chart = (
-    alt.Chart(data)
-    .mark_line(opacity=0.8, clip=True)
-    .encode(
-        x="Date:T",
-        y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin,ymax])),
-        color='Name:N'
+    tickers = {
+        'apple':'AAPL',
+        'facebook':'META',
+        'google':'GOOGL',
+        'microsoft':'MSFT',
+        'netflix':'NFLX',
+        'amazon':'AMZN'
+    }
+
+    df = get_data(days, tickers)
+    companies = st.multiselect(
+        '会社名を選択してください',
+        list(df.index),
+        ['google','amazon','apple','facebook']
+    )
+
+    if not companies:
+        st.error('少なくとも1社は選んでください')
+    else:
+        data = df.loc[companies]
+        st.write("### 株価(USD)", data.sort_index())
+        data = data.T.reset_index()
+        data = pd.melt(data, id_vars=['Date']).rename(
+            columns={'value': 'Stock Prices(USD)'}
         )
+
+        chart = (
+        alt.Chart(data)
+        .mark_line(opacity=0.8, clip=True)
+        .encode(
+            x="Date:T",
+            y=alt.Y("Stock Prices(USD):Q", stack=None, scale=alt.Scale(domain=[ymin,ymax])),
+            color='Name:N'
+            )
+        )
+        st.altair_chart(chart, use_container_width=True)
+except:
+    st.error(
+        "Oh no! めっちゃえらーやねん！ なんでやねん！！"
     )
-    st.altair_chart(chart, use_container_width=True)
-
-
 
 '''
 def get_info(tickers):
@@ -87,5 +91,4 @@ def get_info(tickers):
         action = company.actions
         company.dividends.plot()
         company.actions['Stock Splits'].plot()
-
 '''
