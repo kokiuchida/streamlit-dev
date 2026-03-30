ed0815572bef79e924d930aeaaee6a475b247f31
kokiuchida <ucchii84@gmail.com>
2026-03-30 23:39:36 +0900

    temp save

--- stock.py
+++ stock.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 import streamlit as st
+import altair as alt
 
 days = 20
 tickers = {
@@ -25,14 +26,25 @@ def get_data(days, tickers):
         hist.index.name = 'Name'
         df = pd.cancat([df, hist])
     return df
+    st.write(df)
 
-st.write(df)
+df = pd.DataFrame()
 
+companies = ['apple', 'facebook']
+data = df.loc[companies]
+data.sort_index()
+data.T.reset_index()
 
+data = pd.melt(data, id_vars=['Date'])
 
+st.write(data)
+
+'''
 def get_info(tickers):
     for company in tickers.keys():
         info = company.info
         action = company.actions
         company.dividends.plot()
         company.actions['Stock Splits'].plot()
+
+'''
